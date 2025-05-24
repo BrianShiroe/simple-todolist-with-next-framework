c683a0bd28c4acd751be5fa3f158a814021653c9
Brian Haw <brianshiroe@gmail.com>
2025-05-24 22:11:55 +0800

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOSTMAIN,
+  port: process.env.MYSQLPORTMAIN ? Number(process.env.MYSQLPORTMAIN) : undefined,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
