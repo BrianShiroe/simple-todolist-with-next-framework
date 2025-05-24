06c3780e109fcb37b41751779922c64b1611afb3
Brian Haw <brianshiroe@gmail.com>
2025-05-24 20:44:25 +0800

    railway connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,7 @@ import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
