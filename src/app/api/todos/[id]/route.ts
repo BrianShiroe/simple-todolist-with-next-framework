899f89f0127708ad0902150c98904124634800be
Brian Haw <brianshiroe@gmail.com>
2025-05-24 21:13:47 +0800

    Update route.ts

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,22 +2,13 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
   const { completed } = await req.json();
-  await db.query("UPDATE todos SET completed = ? WHERE id = ?", [
-    completed,
-    context.params.id,
-  ]);
+  await db.query("UPDATE todos SET completed = ? WHERE id = ?", [completed, params.id]);
   return NextResponse.json({ message: "Updated" });
 }
 
-export async function DELETE(
-  req: NextRequest,
-  context: { params: { id: string } }
-) {
-  await db.query("DELETE FROM todos WHERE id = ?", [context.params.id]);
+export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
+  await db.query("DELETE FROM todos WHERE id = ?", [params.id]);
   return NextResponse.json({ message: "Deleted" });
 }
